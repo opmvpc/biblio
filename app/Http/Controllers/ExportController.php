e49a001770fa79325bacbc1ae80141e809e3209b
Thibault Six <t.six@guest.uliege.be>
2020-04-22 21:58:49 +0200

    génération des bibtex manquants

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -18,13 +18,14 @@ class ExportController extends Controller
     public function getBibtex()
     {
         $articles = Article::where('pertinence', 3)->get();
-        // dump($articles);
 
         return  $articles->reduce(function ($text, $article) {
             $bibtex = '';
 
             if ($article->bibtex !== null) {
                 $bibtex = $this->formatBibtexNotNull($article);
+            } else {
+                $bibtex = $this->formatBibtexNull($article);
             }
 
             return  $text .'<br><br>'. $bibtex;
@@ -39,6 +40,35 @@ class ExportController extends Controller
         $parser->parseString($article->bibtex);
         $bibtex = $listener->export();
 
+        $bibtex[0]['citation-key'] = $this->getReference($article);
+        $bibtex[0]['month'] = $article->date->format('m');
+
+        return $parser->convertIntoBibTex($bibtex);
+    }
+
+    public function formatBibtexNull(Article $article): string
+    {
+        $parser = new Parser();
+
+        $bibtex[] = [
+            'citation-key' => $this->getReference($article),
+            'year' => $article->date->format('Y'),
+            'month' => $article->date->format('m'),
+            'titre' => $article->titre,
+            'auteurs' => $article->auteurs->pluck('nom')->implode(', '),
+            'date' => $article->date->format('Y-m-d'),
+            'url' => $article->url,
+            'abstract' => $article->abstract,
+            'keywords' => $article->keywords->pluck('nom')->implode(', '),
+            'doi' => $article->doi,
+            'type' => $article->type->nom,
+        ];
+
+        return $parser->convertIntoBibTex($bibtex);
+    }
+
+    public function getReference($article)
+    {
         $reference = '';
         if ($article->reference != null) {
             $reference = $article->reference;
@@ -48,9 +78,7 @@ class ExportController extends Controller
             $reference = Str::slug(substr($article->titre, 0, 50));
             $reference = preg_replace('/\(|\)/', '', $reference);
         }
-        $bibtex[0]['citation-key'] = $reference;
-        $bibtex[0]['month'] = $article->date->format('m');
 
-        return $parser->convertIntoBibTex($bibtex);
+        return $reference;
     }
 }
